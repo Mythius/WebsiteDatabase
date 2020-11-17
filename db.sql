04abe3c39cc0a91e1134a3947038803382cf8979
Emma <southwickmatthias@gmail.com>
2020-11-17 15:02:32 -0700

    Added Tables to db.sql

---
+++ db.sql
@@ -0,0 +1,26 @@
+DROP DATABASE WEBDATA;
+CREATE DATABASE WEBDATA;
+USE WEBDATA;
+
+CREATE TABLE players(
+	id int NOT NULL,
+	name varchar(25),
+	pswd varchar(25),
+	PRIMARY KEY(id)
+);
+
+CREATE TABLE games(
+	id int NOT NULL,
+	name varchar(25) NOT NULL,
+	PRIMARY KEY (id)
+)
+
+CREATE TABLE scores(
+	id int NOT NULL,
+	player_id int NOT NULL,
+	game_id int NOT NULL,
+	score int NOT NULL,
+	PRIMARY KEY (id),
+	FOREIGN KEY (player_id) REFERENCES players(id),
+	FOREIGN KEY (game_id) REFERENCES games(id)
+)
