05d3132b8ba80191182fd5dd2a706ccea8504bf8
Emma <southwickmatthias@gmail.com>
2020-11-17 15:19:49 -0700

    Add Auto_Incrament to Database

--- db.sql
+++ db.sql
@@ -3,20 +3,20 @@ CREATE DATABASE WEBDATA;
 USE WEBDATA;
 
 CREATE TABLE players(
-	id int NOT NULL,
+	id int NOT NULL AUTO_INCREMENT,
 	name varchar(25),
 	pswd varchar(25),
 	PRIMARY KEY(id)
 );
 
 CREATE TABLE games(
-	id int NOT NULL,
+	id int NOT NULL AUTO_INCREMENT,
 	name varchar(25) NOT NULL,
 	PRIMARY KEY (id)
 );
 
 CREATE TABLE scores(
-	id int NOT NULL,
+	id int NOT NULL AUTO_INCREMENT,
 	player_id int NOT NULL,
 	game_id int NOT NULL,
 	score int NOT NULL,
